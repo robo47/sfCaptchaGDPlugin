9227e3a5a8208bcdeecd6ce8c89b0edd3b11e69c
Benjamin Steininger <robo47@robo47.net>
2011-07-14 11:41:38 +0200

    1.4.5

--- lib/widget/sfWidgetCaptchaGD.class.php
+++ lib/widget/sfWidgetCaptchaGD.class.php
@@ -11,6 +11,6 @@ class sfWidgetCaptchaGD extends sfWidgetForm
     $url = $context->getRouting()->generate("sf_captchagd");
     $value = $context->getRequest()->getPostParameter('captcha');
     $attributes = array_merge($attributes, array('class' => 'captcha'));
-    return $this->renderTag('input', array_merge(array('type' => 'text', 'name' => $name, 'value' => $value), $attributes)) . "<a href='' onClick='return false' title='Reload image'><img src='$url?".time()."' onClick='this.src=\"$url?r=\" + Math.random() + \"&reload=1\"' border='0' class='captcha' /></a>";
+    return $this->renderTag('input', array_merge(array('type' => 'text', 'name' => $name, 'value' => $value), $attributes)) . "<a href='' onClick='return false' title='".__("Reload image")."'><img src='$url?".time()."' onClick='this.src=\"$url?r=\" + Math.random() + \"&amp;reload=1\"' border='0' class='captcha' /></a>";
   }
 }
